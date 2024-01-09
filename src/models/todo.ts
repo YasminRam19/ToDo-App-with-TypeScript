fdda7a09ba5d67efefdbaa50a61438e4c3b14b84
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2024-01-08 20:35:23 -0600

    Data Model added for the Todo, the todos list will be an array of Todo's types instead of an array of strings

---
+++ src/models/todo.ts
@@ -0,0 +1,12 @@
+//Describe how a Todo look like.
+class Todo {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+export default Todo;
