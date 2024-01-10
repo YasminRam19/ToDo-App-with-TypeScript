f84db71fb7fe2f54cba6bca487a7d04f9d6bf3cf
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2024-01-09 20:53:38 -0600

    Form Submission added to insert new tasks in the todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
@@ -6,6 +7,7 @@ function App() {
   const todos = [new Todo("Learn React"), new Todo("Lear TypeScript")];
   return (
     <div>
+      <NewTodo />
       <Todos items={todos} />
     </div>
   );
